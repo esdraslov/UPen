f80b2f4db68f658ab97a6395070715e697cd0c55
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-08-24 17:25:08 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-function open(url){window.open(url);};function getBarValue(bar){search = bar.value ? searchBar.value =! null : "nothing found";console.log(search);};let searchBar = document.getElementById("search");let search = searchBar.value ? searchBar.value =! null : "nothing found";
+function open(url){window.open(url);};function getBarValue(bar){search = bar.value ? bar.value != null : "nothing found";console.log(search);};let searchBar = document.getElementById("search");let search = searchBar.value ? searchBar.value =! null : "nothing found";
