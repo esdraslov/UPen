450fda1d51221e0d4bb329c7b47c0906af49b43f
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-08-24 17:36:10 -0300

    sd

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ function open(url){
     window.open(url);
 };
 function getBarValue(bar){
-    search = bar.value
+    search = bar.value;
     console.log(search);
 };
 let searchBar = document.getElementById("search");
-let search = searchBar.value
+let search = searchBar.value;
